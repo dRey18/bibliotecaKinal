71dae028c5c7d22ebb900ed006405dffa93ab11d
dRey18 <solares054@gmail.com>
2020-09-05 22:21:50 -0600

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LibrosComponent} from './components/libros.component';
-import { HomeComponent } from './home/home.component';
 import { LibrosKComponent } from './libros-k/libros-k.component';
 import { DemoComponent } from './demo/demo.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BannerComponent } from './banner/banner.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LibroDetailComponent } from './libro-detail/libro-detail.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -20,13 +18,11 @@ import { LayoutComponent } from './layout/layout.component';
   declarations: [
     AppComponent,
     LibrosComponent,
-    HomeComponent,
     LibrosKComponent,
     DemoComponent,
     AboutUsComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent,
     PageNotFoundComponent,
     LibroDetailComponent,
     LayoutComponent
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import {} fro
+import { NgModule } from '@angular/core';
+
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeComponent } from './home.component';
+
+
+@NgModule({
+    declarations: [
+        BannerComponent,
+        HomeComponent,
+    ]
+})
+export class HomeModule{
+
+}
